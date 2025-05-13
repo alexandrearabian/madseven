8a4d214b3c43e4b62fc9e72f7ce175b63ef3ba30
Alex <alexandre.arabian.j@gmail.com>
2025-05-13 21:51:09 +0200

    delete acciones de prensa

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 import { useState } from "react";
 import {
   NavigationMenu,
-  NavigationMenuContent,
+  // NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuList,
-  NavigationMenuTrigger,
+  // NavigationMenuTrigger,
   NavigationMenuLink,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
@@ -24,27 +24,27 @@ export function Navbar() {
 
   const navItems = [
     { href: "/acerca-de-nosotros", label: "Acerca de nosotros" },
-    {
-      label: "Acciones de prensa",
-      items: [
-        {
-          href: "/acciones-de-prensa",
-          label: "Acciones de prensa",
-          description:
-            "Descubre nuestras últimas acciones y cobertura mediática.",
-        },
-        {
-          href: "/acciones-de-prensa/noticias",
-          label: "Noticias",
-          description: "Últimas noticias y artículos sobre nuestras acciones.",
-        },
-        {
-          href: "/acciones-de-prensa/eventos",
-          label: "Eventos",
-          description: "Próximos eventos y conferencias de prensa.",
-        },
-      ],
-    },
+    // {
+    //   label: "Acciones de prensa",
+    //   items: [
+    //     {
+    //       href: "/acciones-de-prensa",
+    //       label: "Acciones de prensa",
+    //       description:
+    //         "Descubre nuestras últimas acciones y cobertura mediática.",
+    //     },
+    //     {
+    //       href: "/acciones-de-prensa/noticias",
+    //       label: "Noticias",
+    //       description: "Últimas noticias y artículos sobre nuestras acciones.",
+    //     },
+    //     {
+    //       href: "/acciones-de-prensa/eventos",
+    //       label: "Eventos",
+    //       description: "Próximos eventos y conferencias de prensa.",
+    //     },
+    //   ],
+    // },
     {
       label: "Servicios",
       href: "/servicios",
@@ -58,17 +58,26 @@ export function Navbar() {
 
   const DesktopNav = () => (
     <NavigationMenu className="hidden md:flex md:justify-between top-0 left-0 z-50 w-full shadow-md backdrop-blur max-w-full fixed px-8">
-      <Link
-        href="/"
-        className="hover:scale-105 transition-all duration-300"
-        legacyBehavior
-      >
-        <Image src="/madseven-white.png" alt="Logo" width={160} height={160} />
-      </Link>
-      <NavigationMenuList className="flex justify-between w-full h-16 gap-4">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link
+            href="/"
+            className="hover:scale-105 transition-all duration-300"
+          >
+            <Image
+              src="/madseven-white.png"
+              alt="Logo"
+              width={160}
+              height={160}
+            />
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+
+      <NavigationMenuList className="flex justify-between h-16 gap-4">
         {navItems.map((item, index) => (
           <NavigationMenuItem key={index}>
-            {item.items ? (
+            {/* {item.items ? (
               <>
                 <NavigationMenuTrigger>{item.label}</NavigationMenuTrigger>
                 <NavigationMenuContent>
@@ -79,7 +88,6 @@ export function Navbar() {
                           <Link
                             href={subItem.href}
                             className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent/10 hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                            legacyBehavior
                           >
                             <div className="text-sm font-medium leading-none">
                               {subItem.label}
@@ -94,13 +102,13 @@ export function Navbar() {
                   </ul>
                 </NavigationMenuContent>
               </>
-            ) : (
-              <Link href={item.href} legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  {item.label}
-                </NavigationMenuLink>
-              </Link>
-            )}
+            ) : ( */}
+            <Link href={item.href} legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                {item.label}
+              </NavigationMenuLink>
+            </Link>
+            {/* )} */}
           </NavigationMenuItem>
         ))}
       </NavigationMenuList>
@@ -110,11 +118,7 @@ export function Navbar() {
   const MobileNav = () => (
     <>
       <div className="md:hidden fixed top-0 left-0 w-full z-50 flex justify-between items-center bg-background/40 backdrop-blur-sm ">
-        <Link
-          href="/"
-          className="hover:scale-105 transition-all"
-          legacyBehavior
-        >
+        <Link href="/" className="hover:scale-105 transition-all">
           <Image
             src="/madseven-white.png"
             alt="Logo"
@@ -126,80 +130,77 @@ export function Navbar() {
           <Menu className="h-8 w-8" />
         </button>
       </div>
-      {isOpen && (
-        <>
-          <AnimatePresence>
-            {isOpen && (
-              <>
-                <motion.div
-                  className="fixed inset-0 z-40 bg-background/10 backdrop-blur-md transition-colors md:hidden"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.3 }}
-                  onClick={toggleSidebar}
-                />
 
-                <motion.div
-                  className="bg-background/80 fixed bottom-0 left-1/2 z-50 h-[70vh] w-[90%] max-w-md -translate-x-1/2 overflow-y-auto rounded-t-md p-4 px-8 shadow-md md:hidden"
-                  initial={{ y: "100%", opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                  exit={{ y: "100%", opacity: 0 }}
-                  transition={{
-                    type: "tween",
-                    ease: [0.25, 0.8, 0.25, 1],
-                    duration: 0.4,
-                  }}
-                >
-                  <nav className="flex flex-col space-y-4 mt-4">
-                    {navItems.map((item, index) => (
-                      <motion.div
-                        key={index}
-                        initial={{ opacity: 0, x: -20 }}
-                        animate={{ opacity: 1, x: 0 }}
-                        transition={{ duration: 0.2, delay: index * 0.1 }}
-                      >
-                        {item.items ? (
-                          <div className="space-y-2">
-                            <p className="text-md">{item.label}</p>
-                            {item.items.map((subItem, subIndex) => (
-                              <motion.div
-                                key={subIndex}
-                                initial={{ opacity: 0, x: -20 }}
-                                animate={{ opacity: 1, x: 0 }}
-                                transition={{
-                                  duration: 0.2,
-                                  delay: subIndex * 0.1,
-                                }}
-                              >
-                                <Link
-                                  href={subItem.href}
-                                  className="block pl-8 py-2 text-sm text-muted-foreground hover:text-foreground"
-                                  onClick={toggleSidebar}
-                                >
-                                  {subItem.label}
-                                </Link>
-                              </motion.div>
-                            ))}
-                          </div>
-                        ) : (
-                          <Link
-                            href={item.href}
-                            className="block py-2 text-md"
-                            onClick={toggleSidebar}
+      <AnimatePresence>
+        {isOpen && (
+          <>
+            <motion.div
+              className="fixed inset-0 z-40 bg-background/10 backdrop-blur-md transition-colors md:hidden"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              onClick={toggleSidebar}
+            />
+
+            <motion.div
+              className="bg-background/80 fixed bottom-0 left-1/2 z-50 h-[70vh] w-[90%] max-w-md -translate-x-1/2 overflow-y-auto rounded-t-md p-4 px-8 shadow-md md:hidden"
+              initial={{ y: "100%", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: "100%", opacity: 0 }}
+              transition={{
+                type: "tween",
+                ease: [0.25, 0.8, 0.25, 1],
+                duration: 0.4,
+              }}
+            >
+              <nav className="flex flex-col space-y-4 mt-4">
+                {navItems.map((item, index) => (
+                  <motion.div
+                    key={index}
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.2, delay: index * 0.1 }}
+                  >
+                    {/* {item.items ? (
+                      <div className="space-y-2">
+                        <p className="text-md">{item.label}</p>
+                        {item.items.map((subItem, subIndex) => (
+                          <motion.div
+                            key={subIndex}
+                            initial={{ opacity: 0, x: -20 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{
+                              duration: 0.2,
+                              delay: subIndex * 0.1,
+                            }}
                           >
-                            {item.label}
-                          </Link>
-                        )}
-                      </motion.div>
-                    ))}
-                  </nav>
-                </motion.div>
-              </>
-            )}
-          </AnimatePresence>
-        </>
-      )}
+                            <Link
+                              href={subItem.href}
+                              className="block pl-8 py-2 text-sm text-muted-foreground hover:text-foreground"
+                              onClick={toggleSidebar}
+                            >
+                              {subItem.label}
+                            </Link>
+                          </motion.div>
+                        ))}
+                      </div>
+                    ) : ( */}
+                    <Link
+                      href={item.href}
+                      className="block py-2 text-md"
+                      onClick={toggleSidebar}
+                    >
+                      {item.label}
+                    </Link>
+                    {/* )} */}
+                  </motion.div>
+                ))}
+              </nav>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
     </>
   );
 
